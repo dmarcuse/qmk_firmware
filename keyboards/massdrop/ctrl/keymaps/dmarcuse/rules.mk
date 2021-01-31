e9235e448a3fa69adc3a43768ebdfcb71f45f8ec
Dana Marcuse <dana@marcuse.us>
2021-01-31 16:30:38 -0500

    Enable debouncing for just my layout

--- keyboards/massdrop/ctrl/keymaps/dmarcuse/rules.mk
+++ keyboards/massdrop/ctrl/keymaps/dmarcuse/rules.mk
@@ -1,3 +1,7 @@
 # This keymap requires Massdrop Configurator support
 OPT_DEFS += -DUSE_MASSDROP_CONFIGURATOR
 UNICODE_ENABLE = yes
+
+# Enable debouncing
+DEBOUNCE_TYPE = sym_eager_pk
+DEBOUNCE = 7
